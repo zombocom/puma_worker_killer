389ad92cc9e631cdb4e7418b9fd4ad3f3ca5aeb9
schneems <richard.schneeman@gmail.com>
2014-02-12 16:38:06 -0800

    Puma Worker Killer works

--- lib/puma_worker_killer/reaper.rb
+++ lib/puma_worker_killer/reaper.rb
@@ -1,30 +1,42 @@
 require 'get_process_mem'
+require 'puma/cluster'
 
 module PumaWorkerKiller
   class Reaper
-    def initialize(max_ram)
+    def initialize(max_ram, master = self.get_master)
       @max_ram = max_ram
-      @master  = ObjectSpace.each_object(Puma::Cluster).map { |obj| obj }.first
+      @master  = master
     end
 
-    def memory(pid)
+    def get_master
+      ObjectSpace.each_object(Puma::Cluster).map { |obj| obj }.first
+    end
+
+    def get_memory(pid)
       GetProcessMem.new(pid).mb
     end
 
-    def reap
-      puts "reaping master: #{@master.inspect}"
-      return unless @master
+    def get_workers
       workers = {}
-      @master.workers.each { |worker| workers[worker] = memory(worker.pid) }
-      master_memory = memory(Process.pid)
-      total_memory  = workers.map {|_, memory| memory }.inject(&:+) + master_memory
+      @master.instance_variable_get("@workers").each { |worker| workers[worker] = get_memory(worker.pid) }
+      workers
+    end
 
-      if total_memory > @max_ram
+    def get_total_memory(workers = self.get_workers)
+      master_memory = get_memory(Process.pid)
+      worker_memory = workers.map {|_, mem| mem }.inject(&:+) || 0
+      worker_memory + master_memory
+    end
+
+    def reap
+      return false unless @master
+      workers      = get_workers
+      total_memory = get_total_memory(workers)
+      if workers.any? && total_memory > @max_ram
         biggest_worker, memory_used = workers.sort_by {|_, mem| mem }.last
         biggest_worker.term
-        puts "Out of memory. #{workers.count} workers consuming total: #{total_memory} mb out of max: #{@max_ram} mb. Sending TERM to #{biggest_worker.inspect} consuming #{memory_used} mb."
+        @master.log "PumaWorkerKiller: Out of memory. #{workers.count} workers consuming total: #{total_memory} mb out of max: #{@max_ram} mb. Sending TERM to #{biggest_worker.inspect} consuming #{memory_used} mb."
         Process.wait(biggest_worker.pid)
-        reap # call again in case still out of memory
       end
     end
   end
