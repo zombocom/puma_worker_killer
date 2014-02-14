1b8d103a0cb809833ba14e6580af774e1063df56
schneems <richard.schneeman@gmail.com>
2014-02-13 17:11:14 -0800

    be more explicit about test behavior

--- test/puma_worker_killer_test.rb
+++ test/puma_worker_killer_test.rb
@@ -29,15 +29,20 @@ class PumaWorkerKillerTest < Test::Unit::TestCase
     ram     = 75 #mb
     cluster = FakeCluster.new
     reaper  = PumaWorkerKiller::Reaper.new(ram, cluster)
-    worker_count = 20
-    worker_count.times { cluster.add_worker }
+    worker_count = 0
+    while reaper.get_total_memory < ram * 0.80
+      cluster.add_worker
+      worker_count += 1
+    end
     assert_equal worker_count, cluster.workers.count
-    refute cluster.workers.detect {|w| w.is_term? }
 
     reaper.reap
     assert_equal 0, cluster.workers.select {|w| w.is_term? }.count
     ENV['CAUSE_MEMORY_LEAK'] = "true"
-    sleep 5
+
+    while reaper.get_total_memory < ram
+      sleep 1
+    end
 
     reaper.reap
     assert_equal 1, cluster.workers.select {|w| w.is_term? }.count
