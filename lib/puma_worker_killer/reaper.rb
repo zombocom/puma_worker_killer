373fd2acfd960fdd2fdb476a76eb4b992161d8a1
schneems <richard.schneeman@gmail.com>
2016-06-13 15:27:38 -0500

    Prefer if over unless for clearer code

--- lib/puma_worker_killer/reaper.rb
+++ lib/puma_worker_killer/reaper.rb
@@ -11,7 +11,7 @@ module PumaWorkerKiller
     end
 
     def reap
-      return false unless @cluster.running?
+      return false if @cluster.workers_stopped?
       if (total = get_total_memory) > @max_ram
         @cluster.master.log "PumaWorkerKiller: Out of memory. #{@cluster.workers.count} workers consuming total: #{total} mb out of max: #{@max_ram} mb. Sending TERM to pid #{@cluster.largest_worker.pid} consuming #{@cluster.largest_worker_memory} mb."
         @cluster.term_largest_worker
