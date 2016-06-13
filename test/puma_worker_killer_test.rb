c8d21a3061ef5867a2ed0e606e4f4e455ec9c95b
schneems <richard.schneeman@gmail.com>
2016-06-13 15:28:07 -0500

    Prefer wait_for_it tests

--- test/puma_worker_killer_test.rb
+++ test/puma_worker_killer_test.rb
@@ -4,7 +4,17 @@ class PumaWorkerKillerTest < Test::Unit::TestCase
 
   def test_starts
     port     = 0 # http://stackoverflow.com/questions/200484/how-do-you-find-a-free-tcp-server-port-using-ruby
-    command  = "bundle exec puma #{ fixture_path.join("default.ru") } -t 1:1 -w 5 --preload --debug -p #{ port }"
+    command  = "bundle exec puma #{ fixture_path.join("default.ru") } -t 1:1 -w 2 --preload --debug -p #{ port }"
+    options  = { wait_for: "PumaWorkerKiller:", timeout: 5, env: { "PUMA_FREQUENCY" => 1 } }
+
+    WaitForIt.new(command, options) do |spawn|
+      #
+    end
+  end
+
+  def test_starts
+    port     = 0 # http://stackoverflow.com/questions/200484/how-do-you-find-a-free-tcp-server-port-using-ruby
+    command  = "bundle exec puma #{ fixture_path.join("default.ru") } -t 1:1 -w 2 --preload --debug -p #{ port }"
     options  = { wait_for: "PumaWorkerKiller:", timeout: 5, env: { "PUMA_FREQUENCY" => 1 } }
 
     WaitForIt.new(command, options) do |spawn|
@@ -15,57 +25,12 @@ class PumaWorkerKillerTest < Test::Unit::TestCase
   def test_kills_large_app
     file     = fixture_path.join("big.ru")
     port     = 0
-    command  = "bundle exec puma #{ file } -t 1:1 -w 5 --preload --debug -p #{ port }"
+    command  = "bundle exec puma #{ file } -t 1:1 -w 2 --preload --debug -p #{ port }"
     options  = { wait_for: "PumaWorkerKiller:", timeout: 5, env: { "PUMA_FREQUENCY" => 1, 'PUMA_RAM' => 1} }
 
     WaitForIt.new(command, options) do |spawn|
       assert_contains(spawn, "Out of memory")
     end
-  end
-
-  def test_worker_reaped
-    ram      = 1 #mb
-    cluster  = FakeCluster.new
-    reaper   = PumaWorkerKiller::Reaper.new(ram, cluster)
-    worker_count = 10
-    worker_count.times { cluster.add_worker }
-
-    assert_equal worker_count, cluster.workers.count
-    refute cluster.workers.detect {|w| w.is_term? }
-
-    reaper.reap
-    assert_equal 1, cluster.workers.select {|w| w.is_term? }.count
-
-    reaper.reap
-    assert_equal 2, cluster.workers.select {|w| w.is_term? }.count
-
-    reaper.reap
-    assert_equal 3, cluster.workers.select {|w| w.is_term? }.count
-  ensure
-    cluster.workers.map(&:term)
-  end
-
-  def test_kills_memory_leak
-    ram     = rand(75..100) #mb
-    cluster = FakeCluster.new
-    reaper  = PumaWorkerKiller::Reaper.new(ram, cluster)
-    while reaper.get_total_memory < (ram * 0.80)
-      cluster.add_worker
-      sleep 0.01
-    end
-
-    reaper.reap
-    assert_equal 0, cluster.workers.select {|w| w.is_term? }.count
-
-    until reaper.get_total_memory > ram
-      cluster.add_worker
-      sleep 0.01
-    end
-
-    reaper.reap
-    assert_equal 1, cluster.workers.select {|w| w.is_term? }.count
-  ensure
-    cluster.workers.map(&:term)
   end
 
   def assert_contains(spawn, string)
@@ -76,7 +41,7 @@ class PumaWorkerKillerTest < Test::Unit::TestCase
 
     file     = fixture_path.join("rolling_restart.ru")
     port     = 0
-    command  = "bundle exec puma #{ file } -t 1:1 -w 5 --preload --debug -p #{ port }"
+    command  = "bundle exec puma #{ file } -t 1:1 -w 2 --preload --debug -p #{ port }"
     puts command.inspect
     options  = { wait_for: "booted", timeout: 10, env: { } }
 
