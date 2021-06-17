b488fb11100a9082359794b6bb56c6dffa598a1e
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2021-06-17 11:36:42 +0200

    Parser friendly, more complete logs
    Reaper status logs are printed even if we are killing a worker, and they
    show information for each worker. Information is presented in a key=value
    fashion which is way easier to parse, and correctly handled by most log
    analysis tools.
    
    Since this is breaking former log parsing, I've taken the liberty to
    also clarify logs, showing that master is included in the total.
    
    Signed-off-by: Ulysse Buonomo <buonomo.ulysse@gmail.com>

--- lib/puma_worker_killer/reaper.rb
+++ lib/puma_worker_killer/reaper.rb
@@ -21,23 +21,30 @@ module PumaWorkerKiller
       total = get_total_memory
       @on_calculation&.call(total)
 
-      if total > @max_ram
-        @cluster.master.log "PumaWorkerKiller: Out of memory. #{@cluster.workers.count} workers consuming total: #{total} mb out of max: #{@max_ram} mb. Sending TERM to pid #{@cluster.largest_worker.pid} consuming #{@cluster.largest_worker_memory} mb."
-
-        # Fetch the largest_worker so that both `@pre_term` and `term_worker` are called with the same worker
-        # Avoids a race condition where:
-        #   Worker A consume 100 mb memory
-        #   Worker B consume 99 mb memory
-        #   pre_term gets called with Worker A
-        #   A new request comes in, Worker B takes it, and consumes 101 mb memory
-        #   term_largest_worker (previously here) gets called and terms Worker B (thus not passing the about-to-be-terminated worker to `@pre_term`)
-        largest_worker = @cluster.largest_worker
-        @pre_term&.call(largest_worker)
-        @cluster.term_worker(largest_worker)
-
-      elsif @reaper_status_logs
-        @cluster.master.log "PumaWorkerKiller: Consuming #{total} mb with master and #{@cluster.workers.count} workers."
+      if @reaper_status_logs
+        @cluster.master.log 'PumaWorkerKiller: Status log. ' \
+          "total=#{total}mb " \
+          "master=#{@cluster.master_memory}md " \
+          "worker_count=#{@cluster.workers.count} " \
+          "#{@cluster.workers.map { |worker, mem| "worker_#{worker.pid}=#{mem}mb" } * " "}"
       end
+
+      return unless total > @max_ram
+
+      @cluster.master.log "PumaWorkerKiller: Out of memory. #{@cluster.workers.count} " \
+        "workers and master consuming total: #{total} mb out of max: #{@max_ram} mb. " \
+        "Sending TERM to pid #{@cluster.largest_worker.pid} consuming #{@cluster.largest_worker_memory} mb."
+
+      # Fetch the largest_worker so that both `@pre_term` and `term_worker` are called with the same worker
+      # Avoids a race condition where:
+      #   Worker A consume 100 mb memory
+      #   Worker B consume 99 mb memory
+      #   pre_term gets called with Worker A
+      #   A new request comes in, Worker B takes it, and consumes 101 mb memory
+      #   term_largest_worker (previously here) gets called and terms Worker B (thus not passing the about-to-be-terminated worker to `@pre_term`)
+      largest_worker = @cluster.largest_worker
+      @pre_term&.call(largest_worker)
+      @cluster.term_worker(largest_worker)
     end
   end
 end
